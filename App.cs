bcee640f850de9bc8940b7eb84303121f5af7efb
iwanyaa <241591391+iwanyaa@users.noreply.github.com>
2025-11-13 22:35:29 +0100

    added raylib

---
+++ App.cs
@@ -0,0 +1,20 @@
+﻿using Raylib_cs;
+namespace Cirkit
+{
+    internal class App
+    {
+        static void Main()
+        {
+            Raylib.InitWindow(1280, 720, "Cirkit");
+            Raylib.SetTargetFPS(60);
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Raylib.GetColor(0x000000ff));
+                Raylib.DrawText("Hello, world!", 20, 20, 20, Raylib.GetColor(0xffffffff));
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
